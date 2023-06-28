2045f9f502dfcd825f57fed61f112ffaef2c118a
JToquica <jose.herley.2010@gmail.com>
2023-06-28 10:46:27 -0500

    Cambios ruta y APP_SHELL SW

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const INMUTABLE_CHACHE_NAME = "inmutable-v1";
 const DYNAMIC_CACHE_NAME    = "dynamic-v1";
 
 const APP_SHELL = [
-    "/",
+    // "/",
     "index.html",
     "css/style.css",
     "img/favicon.ico",
